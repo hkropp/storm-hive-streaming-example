a2b35631afc98c36e2c31daed87fa9fac63243b5
hkropp <hkropp@hortonworks.com>
2015-09-13 20:23:50 +0200

    Flux topology example

---
+++ deploy_vagrant.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mvn clean package
+
+vagrant_ssh_port="-P 2222"
+vagrant_ssh_key="-i /Users/hkropp/.vagrant.d/insecure_private_key"
+
+scp $vagrant_ssh_port $vagrant_ssh_key target/storm-hive-streaming-example-1.0-SNAPSHOT.jar vagrant@localhost:
+scp $vagrant_ssh_port $vagrant_ssh_key flux-demo.yaml vagrant@localhost:
+
+scp $vagrant_ssh_port $vagrant_ssh_key src/main/resources/hive.schema vagrant@localhost:
+scp $vagrant_ssh_port $vagrant_ssh_key src/main/resources/create_kafka_topic.sh vagrant@localhost:
+scp $vagrant_ssh_port $vagrant_ssh_key run.sh vagrant@localhost:
