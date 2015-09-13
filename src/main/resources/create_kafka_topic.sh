46c58944d1027f3e131a90265977755ea53d7ae2
hkropp <hkropp@hortonworks.com>
2015-09-13 20:24:49 +0200

    Make use of current to support different HDP versions

--- src/main/resources/create_kafka_topic.sh
+++ src/main/resources/create_kafka_topic.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/hdp/2.2.0.0-2041/kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic stock_topic
+/usr/hdp/current/kafka-broker/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic stock_topic
